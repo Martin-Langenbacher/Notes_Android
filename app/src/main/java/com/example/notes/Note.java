fd6bce3a5ba3c640b5f3b274d3d386e2de3b9298
Martin Langenbacher <martin.langenbacher@googlemail.com>
2020-03-10 10:21:38 +0100

    3

--- app/src/main/java/com/example/notes/Note.java
+++ app/src/main/java/com/example/notes/Note.java
@@ -10,7 +10,32 @@ public class Note {
     private int id;
     private String title;
     private String description;
+    //Change name: -->>>' @ColumnInfo(name = "priority_column") '<<<---
     private int priority;
 
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }
---
+++ app/src/main/java/com/example/notes/NoteDao.java
@@ -0,0 +1,38 @@
+package com.example.notes;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+// general rule: one Dao per entity...
+@Dao
+public interface NoteDao {
+
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    // no ready made annotation...
+    @Query("DELETE FROM note_table")
+    void deleteAllNotes();
+
+    @Query("SELECT * FROM note_table ORDER BY priority DESC")
+    List<Note> getAllNotes();
+
+
+
+
+
+
+
+}
